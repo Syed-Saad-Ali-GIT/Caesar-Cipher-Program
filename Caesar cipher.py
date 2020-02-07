769142b8b41852a4136a1388289baeac032d3fc0
Saad Ali <41276602+S3717159@users.noreply.github.com>
2020-02-07 19:31:13 +1100

    Python Caesar Cipher Program update

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -1,19 +1,47 @@
-print("Welcome to Caesar Cipher Program")
 
-print("Would You like to encode or decode [e/d]")
+textToEncode = ""
+deocdedText = ""
+
+textToDecode = ""
+encodedText = ""
+
+def encode():
+    print("Encoding")
+    textToEncode = input("What will you like to encode \n")
+
+    incorrectInput = True
+    while(incorrectInput):
+        keyShift = input("What do you want the key to be 1-25 \n")
+        if(int(keyShift) > 25 or int(keyShift) < 1):
+            print("Enter Between 1-25")
+        
+        else:
+            print("Nice Still in Dev... key :" + keyShift)
+            incorrectInput = False
+
+
+def decode(): 
+    print("Decoding")
+    
+
+print("Welcome to Caesar Cipher Program \n")
+
+print("Would You like to encode or decode [e/d] \n")
 
 
 incorrectInput= True
 
 while(incorrectInput):
-    userInput = input("[e/d] pick one please :  ")
+    userInput = input("[e/d] pick one please :  \n")
     if userInput == 'e':
-        print("encoding")
+        encode()
         incorrectInput = False
         
     elif userInput == 'd' :
-        print("decoding")
+        decode()
         incorrectInput = False
 
     else : 
-        print("error...")
+        print("error... \n")
+
+
