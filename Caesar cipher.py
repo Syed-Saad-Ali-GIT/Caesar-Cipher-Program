8fcae0b23691407446c7ec2b6e4046be2f0b4cf2
Saad Ali <41276602+S3717159@users.noreply.github.com>
2020-02-09 19:50:27 +1100

    input correction code for encoding

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -6,18 +6,32 @@ textToDecode = ""
 encodedText = ""
 
 def encode():
-    print("Encoding")
-    textToEncode = input("What will you like to encode \n")
-
     incorrectInput = True
+    print("Encoding")
+
+
     while(incorrectInput):
-        keyShift = input("What do you want the key to be 1-25 \n")
-        if(int(keyShift) > 25 or int(keyShift) < 1):
-            print("Enter Between 1-25")
-        
+        textToEncode = input("What will you like to encode \n")
+        if(textToDecode.isalpha):
+            incorrectInput=False
         else:
-            print("Nice Still in Dev... key :" + keyShift)
-            incorrectInput = False
+            print("Enter Only Alphabets , no numbers or special characters")
+
+            
+    incorrectInput = True       
+    while(incorrectInput):
+        try:
+            keyShift = input("What do you want the key to be 1-25 \n")
+            if(int(keyShift) > 25 or int(keyShift) < 1):
+                print("Enter Between 1-25")
+            
+            else:
+                print("Nice Still in Dev... key :" + keyShift + "\n")
+                print(textToEncode)
+                textToDecode
+                incorrectInput = False
+        except:
+            print("Something Went Wrong :( \n")
 
 
 def decode(): 
